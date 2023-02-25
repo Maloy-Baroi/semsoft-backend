65c90d05dd79292a53c9323e880a869bf226b0bb
Maloy Baroi (XPL) <44752184+Maloy-Baroi@users.noreply.github.com>
2023-02-25 13:30:35 +0600

    Update urls.py

--- semsoft_backend/urls.py
+++ semsoft_backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('dev-admin/', admin.site.urls),
@@ -22,3 +24,6 @@ urlpatterns = [
     path('api-auth/', include('App_login.urls')),
     path('api-main/', include('App_main.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
